b17f14cdbddc838c96b43a2c2355c5e697b4828b
akshatgg <akshatg9636@gmail.com>
2024-04-27 01:07:18 +0545

    adding components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,50 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './App.css'
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Courses from "./Components/Courses/Courses";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Error from "./Error/Error";
+import OutletP from "./Outlet/OutletP";
+import Signin from "./Signin/Signin";
+import Signup from "./Signup/Signup";
 
 function App() {
- 
-  <div>
+  return (
+    
+      <div>
+        <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/About" element={<Navbar />} />
+        <Route path="/Contact" element={<Navbar />} />
+        <Route path="/Courses" element={<Navbar />} />
+        <Route path="/Signup" element={<Navbar />} />
+        <Route path="/Signin" element={<Navbar />} />
 
-  </div>
+
+
+        </Routes>
+    
+
+        <Routes>
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Courses" element={<Courses />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+
+        <Routes>
+          <Route path="/" element={<Footer />} />
+          {/* <Route path="/About" element={<Footer />} /> */}
+          {/* <Route path="/Contact" element={<Footer />} /> */}
+        </Routes>
+      </div>
+  );
 }
 
-export default App
+export default App;
+ 
