0a06574a6e0903254d047c2d602447a6fb015887
akshatgg <akshatg9636@gmail.com>
2024-04-27 02:35:43 +0545

    Home page is ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import About from "./Components/About/About";
-import Contact from "./Components/Contact/Contact";
-import Courses from "./Components/Courses/Courses";
-import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
-import Navbar from "./Components/Navbar/Navbar";
-import Error from "./Error/Error";
-import OutletP from "./Outlet/OutletP";
-import Signin from "./Signin/Signin";
-import Signup from "./Signup/Signup";
+// import About from "./Components/About/About";
+// import Contact from "./Components/Contact/Contact";
+// import Courses from "./Components/Courses/Courses";
+import Footer from "./Components/Footer/Footer.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+// import Error from "./Error/Error";
+// import OutletP from "./Outlet/OutletP";
+// import Signin from "./Signin/Signin";
+// import Signup from "./Signup/Signup";
 
 function App() {
   return (
-    
+    <Router>
       <div>
         <Routes>
         <Route path="/" element={<Navbar />} />
-        <Route path="/About" element={<Navbar />} />
-        <Route path="/Contact" element={<Navbar />} />
-        <Route path="/Courses" element={<Navbar />} />
-        <Route path="/Signup" element={<Navbar />} />
-        <Route path="/Signin" element={<Navbar />} />
+        {/* <Route path="/About" element={<Navbar />} /> */}
+        {/* <Route path="/Contact" element={<Navbar />} /> */}
+        {/* <Route path="/Courses" element={<Navbar />} /> */}
+        {/* <Route path="/Signup" element={<Navbar />} /> */}
+        {/* <Route path="/Signin" element={<Navbar />} /> */}
 
 
 
@@ -29,12 +29,12 @@ function App() {
     
 
         <Routes>
-          <Route path="/Signin" element={<Signin />} />
-          <Route path="/Signup" element={<Signup />} />
+          {/* <Route path="/Signin" element={<Signin />} /> */}
+          {/* <Route path="/Signup" element={<Signup />} /> */}
           <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Courses" element={<Courses />} />
-          <Route path="/Contact" element={<Contact />} />
+          {/* <Route path="/About" element={<About />} /> */}
+          {/* <Route path="/Courses" element={<Courses />} /> */}
+          {/* <Route path="/Contact" element={<Contact />} /> */}
         </Routes>
 
         <Routes>
@@ -43,6 +43,7 @@ function App() {
           {/* <Route path="/Contact" element={<Footer />} /> */}
         </Routes>
       </div>
+      </Router>
   );
 }
 
