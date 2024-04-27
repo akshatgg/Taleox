0f8ea59f34063f45673833ceec4d277294738a35
akshatgg <akshatg9636@gmail.com>
2024-04-28 02:55:39 +0545

    change from axioos to fetch

--- Backend/frontend/src/Components/Signup/Signup.jsx
+++ Backend/frontend/src/Components/Signup/Signup.jsx
@@ -50,8 +50,15 @@ const Signup = () => {
         name: name,
       };
   
-      const response = await Axios.post('http://localhost:5000/api/auth/user/register', userdata);
-  
+     const response = fetch('/api/auth/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userdata),
+      })
+      
+
       if (response.status === 201) {
         alert("Sign up successful");
         console.log("Sign up successful");
