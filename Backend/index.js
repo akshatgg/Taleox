c328f5d8a97d6d8685cb794f6b81a5212d47a434
akshatgg <akshatg9636@gmail.com>
2024-04-27 20:37:08 +0545

    test6

--- Backend/index.js
+++ Backend/index.js
@@ -56,7 +56,7 @@ app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname1, "/frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
 });
 
 
