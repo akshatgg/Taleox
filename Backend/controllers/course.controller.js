d5a8bbc65a44f33e3d82e5f4e9954e59ea24a15c
akshatgg <akshatg9636@gmail.com>
2024-05-08 12:38:17 +0545

    make all concept of getAllcourses and getlecturesbyid function

--- Backend/controllers/course.controller.js
+++ Backend/controllers/course.controller.js
@@ -13,7 +13,7 @@ const getAllCourses=async(req,res,next)=>{
             success:true,
             message:"All courses",
             courses,
-        })
+        });
     }
 
     catch(e){
@@ -26,6 +26,25 @@ const getAllCourses=async(req,res,next)=>{
 }
 
 const getLecturesByCourseId=async(req,res,next)=>{
+try{
+const {id}=req.params;
+console.log(id);
+const course=await Course.findById(id);
+if(!course){
+    return next(new apperror("course id is invalid" ,400));
+}
+console.log(course);
+
+res.status(200).json({
+    success:true,
+    message:"Courses lectures fetched success",
+    lectures: course.lectures
+});
+}
+catch(e){
+    return next(new apperror(e.message,500));
+}
+
 
 }
 
