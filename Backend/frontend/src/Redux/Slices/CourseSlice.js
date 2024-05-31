ca3bda0fc1f785668cce98a7982e6037e8d6defd
Akshat Gupta <akshatg9636@gmail.com>
2024-05-31 16:35:30 +0000

    adding get all courses concept

--- Backend/frontend/src/Redux/Slices/CourseSlice.js
+++ Backend/frontend/src/Redux/Slices/CourseSlice.js
@@ -1,19 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit"
 import axiosInstance from "../../Helper/axiosinstance"
-
+import { toast } from "react-hot-toast";
 
 const initialState={
 coursedata:[]   
-    
-
 }
+
+export const getAllCourses= createAsyncThunk("/course/getallcourses",async()=>{
+    try{
+
+    }
+    catch(e){
+        toast.error(e.message);
+    }
+})
+
 
 const courseslice=createSlice({
     name:'course',
     initialState,
     reducers:{},
     extraReducers:(builder)=>{
-
+      
     }
 })
 
