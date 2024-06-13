58ae8f83755b8e52ff0e91a5dc78f27b69ec4b69
akshatgg <akshatg9636@gmail.com>
2024-06-13 15:13:26 +0545

    adding some functions

---
+++ Backend/frontend/src/Components/User/EditProfile.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+
+
+function EditProfile(){
+ const dispatch=useDispatch()
+ const [previewImage,setImagePreview]=useState("");
+
+   const [data,setdata]=useState({
+    name:"",
+    number:"",
+    avatar:undefined,
+    userID:useSelector((state)=> state?.auth?.data?.__id)
+
+   })
+
+
+   const handlegetImage=(event)=>{
+    event.preventDefault();
+
+    const uploadimage=event.target.files[0];
+
+    if(uploadimage){
+        setdata({
+            ...data,
+            avatar: uploadimage,
+        });
+    }
+     const fileReader=new FileReader();
+     fileReader.readAsDataURL(uploadimage);
+     fileReader.addEventListener("load",function(){
+        setImagePreview(this.result);
+     })
+   }
+
+
+   const handlegetuser=async(e)=>{
+   const {name, value}=e.target; 
+   const newuserdata={
+    ...data,
+    [name]:value
+   }
+
+
+   
+   
+
+}
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+
+export default EditProfile;
